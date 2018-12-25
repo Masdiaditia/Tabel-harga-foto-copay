640e590e50ddf885f5eec12c4f74da5c497773e5
Masdi Aditia <45525790+Masdiaditia@users.noreply.github.com>
2018-12-25 13:18:24 +0700

    Add files via upload

---
+++ Tabel Harga foto Copy.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (void)
+{
+    int a=1,h;
+    printf("Harga foto copy\n\n");
+    printf("jumlah lembar\t| Harga\t\n");
+    while (a<=100)
+
+    {
+        h= a*95;
+        printf ("---------------------_\n");
+        printf("%d  lembar =\t|Rp%d\n",a,h);
+        a++;
+
+    }
+    printf("\n");
+    system("pause");
+    return 0;
+}
